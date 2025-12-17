40dab487a9b6eda98a09bb52341b7fa75e83513d
TypeLuis <typeluis@gmail.com>
2025-12-17 21:31:54 +0000

    upto part 4

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const body = document.body
 
 const main = document.createElement("main")
 const heading = document.createElement('h1')
-const topMenuEl = document.createElement("nav")
+// const topMenuEl = document.createElement("nav")
+const topMenuEl = document.querySelector("#top-menu")
 
 
 heading.textContent = "Dom Manipulation"
@@ -16,10 +17,21 @@ topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
 topMenuEl.classList.add("flex-around")
 
 const menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+        {text: 'all', href: '/catalog/all'},
+        {text: 'top selling', href: '/catalog/top'},
+        {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+        {text: 'new', href: '/orders/new'},
+        {text: 'pending', href: '/orders/pending'},
+        {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+        {text: 'profile', href: '/account/profile'},
+        {text: 'sign out', href: '/account/signout'},
+    ]},
 ];
 
 menuLinks.forEach((obj) => {
@@ -32,5 +44,22 @@ menuLinks.forEach((obj) => {
 
 
 main.appendChild(heading)
-body.appendChild(topMenuEl)
+// body.appendChild(topMenuEl)
 body.appendChild(main)
+
+
+// pt 2
+
+const subMenu = document.querySelector("#sub-menu")
+subMenu.style.height = "100%"
+subMenu.style.backgroundColor = "var(--sub-menu-bg)"
+subMenu.classList.add("flex-around")
+subMenu.style.position = "absolute"
+subMenu.style.top = 0
+
+
+topMenuEl.addEventListener("click", (e) => {
+    if(e.target === topMenuEl) return
+    console.log(e.target)
+    
+})
